8c8f3770a775b3e96feb7e4ea3ee0d560ab63b70
Jakub Grzesiak <grzesiakjakub19@gmail.com>
2022-01-31 22:15:42 +0100

    Tesotwanie

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
+class test:
+    Ustawienie_1= None
+    Ustawienie_2 = None
+    def __init__(self, set1, set2):
+        self.Ustawienie_1 = set1
+        self.Ustawienie_2 = set2
+
+
+
+
 def main():
     print("Hello world")
+
+def gen(name, set1, set2):
+
 
 
 if __name__ == '__main__':
