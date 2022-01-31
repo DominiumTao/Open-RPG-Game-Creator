bc8cc491844e0097300e6fa04a4aa9c9b42d9525
Jakub Grzesiak <grzesiakjakub19@gmail.com>
2022-01-31 23:18:15 +0100

    Added template of data structure in rooms Dictionary

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
+import RoomClass as R
+
+
 def main():
-    print("Hello world")
+    rooms = {
+        1: {"name": "", "Contains": [], "Connects": []}
+    }
+    name = input()
+
+    rooms[2] = {"name": name, "Contains": [1], "Connects": [3]}
+
+    print(rooms)
+
 
 if __name__ == '__main__':
     main()
+    print("koniec")
