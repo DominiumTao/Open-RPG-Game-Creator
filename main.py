2a5030f06a2cdae5f8f5bdc08a770a36295f5358
Jakub Grzesiak <grzesiakjakub19@gmail.com>
2022-02-01 12:21:59 +0100

    Added function for configuration of containers, only early stage template. Change room_gen function.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import RoomClass as R
 
-rooms = {
-}
+rooms = {1: {'name': 'Startowa_Lokacja', 'Contains': [1, 2], 'Connects': [2]}
+         }
 
 
 def room_gen():
@@ -9,7 +9,14 @@ def room_gen():
     id_contains_list = []
     rooms_temp = {}
 
-    def add_conn(rooms_temp):
+    def add_conn(rooms_temp, bool):
+        if bool:
+            temp_dic = rooms_temp[list(rooms_temp.keys())[0]]
+            conn = temp_dic["Connects"]
+            for e_conn in conn:
+                if not list(rooms_temp.keys())[0] in rooms[e_conn]["Connects"]:
+                    rooms[e_conn]["Connects"].append(list(rooms_temp.keys())[0])
+                    print(rooms[e_conn], "tu")
         rooms.update(rooms_temp)
 
     def contains_gen():
@@ -44,13 +51,29 @@ def room_gen():
         id_contains_list = contains_gen()
         id_of_room = list(rooms.keys())[-1] + 1
         rooms_temp[id_of_room] = {"name": name, "Contains": id_contains_list, "Connects": id_connect_list}
-        add_conn(rooms_temp)
+        add_conn(rooms_temp, True)
     else:
         print("**GENERATING FIRST ROOM**")
         id_contains_list = contains_gen()
         rooms_temp[1] = {"name": name, "Contains": id_contains_list, "Connects": id_connect_list}
-        add_conn(rooms_temp)
+        add_conn(rooms_temp, False)
 
+
+def containers_gen():
+    while True:
+        for key in rooms.keys():
+            print("ID of room: ", key, "Name of room: '", rooms[key]["name"], "' Contains of the room: ",
+                  rooms[key]["Contains"],
+                  " Connections to/from of the room: ", rooms[key]["Connects"])
+        print(f"Please provide room id that you want want to config, if you don't want to config any containers please enter 0:  ")
+        room_id = input()
+        # room_id = int(room_id)
+        if room_id.isdecimal() and int(room_id) in rooms.keys() and int(room_id) != 0:
+            print("s")
+        elif room_id.isdecimal() and int(room_id) == 0:
+            break
+        else:
+            print("\nPlease enter only id number example '1', without apostrophe, check if number is correct ID number\n\n" )
 
 def main():
     rooms = {
@@ -64,5 +87,5 @@ def main():
 
 
 if __name__ == '__main__':
-    room_gen()
-    print(rooms)
+    # room_gen()
+    containers_gen()
