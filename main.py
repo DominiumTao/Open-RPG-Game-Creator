517a359c968e43713f8ff7c60d03b0b17e3759b9
Jakub Grzesiak <grzesiakjakub19@gmail.com>
2022-02-01 11:00:28 +0100

    Future updates of rooms_gen dictionary

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import RoomClass as R
 
-rooms = {1: {"name": "Start", "Contains": [1, 2], "Connects": [2, 3]},
-         2: {"name": "Start1", "Contains": [3, 4], "Connects": [1, 2]},
-         3: {"name": "Start2", "Contains": [5, 6], "Connects": [1]}
-         }
+rooms = {
+}
 
 
 def room_gen():
-    print("Please enter the name of the room: ")
-    name = input()
     id_connect_list = []
     id_contains_list = []
+    rooms_temp = {}
+
+    def add_conn(rooms_temp):
+        rooms.update(rooms_temp)
+
+    def contains_gen():
+        id_contains_list_in = []
+        while True:
+            print(
+                "Please provide the id of the contains that should appear in the room, if you want to stop adding connections write 0: ")
+            id_contains = input()
+            if id_contains.isdecimal():
+                if int(id_contains) != 0:
+                    id_contains_list_in.append(int(id_contains))  # Do zmiany
+                if int(id_contains) == 0:
+                    return id_contains_list_in
+
+    print("Please enter the name of the room: ")
+    name = input()
     if rooms:
         print("List of rooms that you can connect to: ")
         for key in rooms.keys():
-            print("Name of room: '", rooms[key]["name"], "' Contains of the room: ", rooms[key]["Contains"],
+            print("ID of room: ", key, "Name of room: '", rooms[key]["name"], "' Contains of the room: ",
+                  rooms[key]["Contains"],
                   " Connections to/from of the room: ", rooms[key]["Connects"])
         while True:
             print(
@@ -22,21 +38,19 @@ def room_gen():
             id_connect = input()
             if id_connect.isdecimal():
                 if int(id_connect) != 0:
-                    id_connect_list.append(int(id_connect)) #Do zmiany
+                    id_connect_list.append(int(id_connect))  # Do zmiany
                 if int(id_connect) == 0:
                     break
-        while True:
-            print(
-                "Please provide the id of the contains that should appear in the room, if you want to stop adding connections write 0: ")
-            id_contains = input()
-            if id_contains.isdecimal():
-                if int(id_contains) != 0:
-                    id_contains_list.append(int(id_contains)) #Do zmiany
-                if int(id_contains) == 0:
-                    break
+        id_contains_list = contains_gen()
         id_of_room = list(rooms.keys())[-1] + 1
-        rooms[id_of_room] = {"name": name, "Contains": id_contains_list, "Connects": id_connect_list}
-        print(rooms)
+        rooms_temp[id_of_room] = {"name": name, "Contains": id_contains_list, "Connects": id_connect_list}
+        add_conn(rooms_temp)
+    else:
+        print("**GENERATING FIRST ROOM**")
+        id_contains_list = contains_gen()
+        rooms_temp[1] = {"name": name, "Contains": id_contains_list, "Connects": id_connect_list}
+        add_conn(rooms_temp)
+
 
 def main():
     rooms = {
@@ -51,3 +65,4 @@ def main():
 
 if __name__ == '__main__':
     room_gen()
+    print(rooms)
